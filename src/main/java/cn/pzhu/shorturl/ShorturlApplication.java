7524f1ea592453b1bf472a7e24de108ec0f8ea0d
jinguomin <1291144341@qq.com>
2019-04-15 11:06:51 +0800

    发布到服务器进行相应修改

--- src/main/java/cn/pzhu/shorturl/ShorturlApplication.java
+++ src/main/java/cn/pzhu/shorturl/ShorturlApplication.java
@@ -2,13 +2,22 @@ package cn.pzhu.shorturl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan("cn.pzhu.shorturl.dao.dao")
 @ComponentScan("cn.pzhu.shorturl")
-public class ShorturlApplication {
+@Configuration
+public class ShorturlApplication extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(ShorturlApplication.class);
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(ShorturlApplication.class, args);
